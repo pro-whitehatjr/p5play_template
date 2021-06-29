6ca9d34ab09d833445102e185e156d1f0653b3ad
tanishahanamsagar <86666421+tanishahanamsagar@users.noreply.github.com>
2021-06-29 20:05:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,33 @@ function setup() {
 function draw() 
 {
   background(30);
+  
+  if(keyIsDown(UP_ARROW)){
+background("green")
 
 }
+
+if(keyIsDown(DOWN_ARROW)){
+  background("red")
+  
+  }
+  
+  if(keyIsDown(RIGHT_ARROW)){
+    background("pink")
+    
+    }
+    
+    if(keyIsDown(LEFT_ARROW)){
+      background("blue")
+      
+      }
+      
+  
+  
+  
+
+}
+
 
 
 

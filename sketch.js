b1d029c8389dbc3f77b77d2c13d88747dd6f1eb0
nisharegil <nisha@binaryrepublik.com>
2021-07-19 21:13:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,6 +6,19 @@ function setup() {
 function draw() 
 {
   background(30);
+  
+  if(keyIsDown(DOWN_ARROW))
+  background("red")
+
+  if(keyIsDown(UP_ARROW))
+  background("green")
+
+  if(keyIsDown(LEFT_ARROW))
+  background("yellow")
+
+  if(keyIsDown(RIGHT_ARROW))
+  background("blue")
+  
 
 }
 
